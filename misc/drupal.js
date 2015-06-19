eb4088f8f6011c29d60382125bd3f4a02e4f0014
Kevin Day <kday@mcneese.edu>
2015-06-19 14:36:56 -0500

    Update Core: drupal 7.38

--- misc/drupal.js
+++ misc/drupal.js
@@ -390,6 +390,9 @@ Drupal.ajaxError = function (xmlhttp, uri) {
 // Class indicating that JS is enabled; used for styling purpose.
 $('html').addClass('js');
 
+// 'js enabled' cookie.
+document.cookie = 'has_js=1; path=/';
+
 /**
  * Additions to jQuery.support.
  */
