324d56986e685d7b72c6b069076ebb131d2301b0
Kevin Day <kday@mcneese.edu>
2017-06-23 08:47:43 -0500

    Update Core: drupal 7.56
    Signed-off-by: Kevin Day <kday@mcneese.edu>

--- misc/drupal.js
+++ misc/drupal.js
@@ -535,6 +535,9 @@ Drupal.ajaxError = function (xmlhttp, uri, customMessage) {
 // Class indicating that JS is enabled; used for styling purpose.
 $('html').addClass('js');
 
+// 'js enabled' cookie.
+document.cookie = 'has_js=1; path=/';
+
 /**
  * Additions to jQuery.support.
  */
